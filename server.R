286ada3ae0a81bff67cb3518fddf3ebd516d74d5
Shobhit <shobhitsingh2007@gmail.com>
2016-12-16 23:41:44 +0530

    Create server.R

---
+++ server.R
@@ -0,0 +1,36 @@
+library(shiny)
+library(leaflet)
+
+function(input, output, session) {
+  output$map <- renderLeaflet({
+    tryCatch({
+      if (input$lat == "" || input$lon == "") {
+        leaflet() %>%
+          addTiles() %>%
+          setView(lng = 35 ,
+                  lat = 26,
+                  zoom = 2)
+        
+      } else {
+        leaflet() %>%
+          addTiles() %>%
+          setView(lng = input$lon ,
+                  lat = input$lat,
+                  zoom = input$zoom) %>%
+          addMarkers(lat = input$lat,
+                     lng = input$lon)
+        
+      }
+      
+    },
+    error = function(e) {
+      output$text1 <- renderText({
+        paste("Not a valid geo-codes")
+      })
+      return(NULL)
+    })
+  })
+  
+  
+  
+}
